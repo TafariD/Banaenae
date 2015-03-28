01ea58c9ca9faf8c0c7144b75a781e0d7819d659
Jason Fan <jason.fan.74@gmail.com>
2015-03-28 18:13:37 -0400

    got html raw

--- backend/scrape.py
+++ backend/scrape.py
@@ -1,4 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib3
+import urllib.request
 
 print("imported!")
+
+base_url = 'http://menus.tufts.edu/foodpro/longmenu.asp?sName=TUFTS+DINING&locationNum=09&locationName=Carmichael+Dining+Center&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=03%2F28%2F2015&mealName=Lunch'
+
+def getFoodNames(url):
+    html = urllib.request.urlopen(url).read()
+    print(html)
+
+print("running scraper")
+getFoodNames(base_url)
