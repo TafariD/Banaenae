4b315d2df08430034c2439d72cf8a0c0b519f912
Ben Battle <benjamin.battle@tufts.eddu>
2015-04-05 19:30:33 -0400

    alltime update

--- backend/scrape.py
+++ backend/scrape.py
@@ -9,34 +9,37 @@ base_url = 'http://menus.tufts.edu/foodpro/longmenu.asp?sName=TUFTS+DINING&locat
 
 #return dictionary containting all foodnames as keys with 0s as vals (for daily)
 def getFoodNames(url):
-	food_dict = {}
+    food_dict = {}
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html)
     data = soup.find_all("div", "longmenucoldispname")#.string
     for div in data:
-#        print(div.string)
         food_dict[div.string] = 0 
     return food_dict
 
 def updateAllTime(daily_dict):
-	with open ("alltime.json", "w") as f:
-			alltime = json.load(f)
-			for item in daily_dict:
-				if (item in alltime):
-					continue
-				else:
-					alltime[item] = 0
-	return alltime				
+    with open ("alltime.json", "r") as f:
+            alltime = json.load(f)
+            for item in daily_dict:
+                if (item in alltime):
+                    continue
+                else:
+                    alltime[item] = 0
+    return alltime              
 
 def main():
-	print("running scraper")
-	
-	food_dict = getFoodNames(base_url)
-	with open ("daily_all.json","w") as f:
-	    json.dump(food_dict, f, sort_keys = True, indent = 4, ensure_ascii = False)
+    print("running scraper")
+    """
+    daily_dict = getFoodNames(base_url)
+    with open ("daily_all.json","w") as f:
+        json.dump(daily_dict, f, sort_keys = True, indent = 4, ensure_ascii = False)
+    """
+    daily_dict = {}
+    with open ("daily_all.json") as f:
+        daily_dict = json.load(f)
 
-	alltime = updateAllTime(food_dict)    
-	with open ("alltime.json","w") as f:
-	    json.dump(alltime, f, sort_keys = True, indent = 4, ensure_ascii = False)
+    alltime = updateAllTime(daily_dict)    
+    with open ("alltime.json","w") as f:
+        json.dump(alltime, f, sort_keys = True, indent = 4, ensure_ascii = False)
 
 main()
