7e09d79c45aaa6ce2adb26779fe42e90ff4a881c
Param Chopra <param012chopra@gmail.com>
2015-04-09 23:01:12 -0400

    key-vals from server parsed and sorted

--- frontend/frontjava.js
+++ frontend/frontjava.js
@@ -1,36 +1,34 @@
-function getdata(){
-    $.getJSON("food_scores2.json", function(jdata) { //code based off of stackoverflow.com/questions/7346563/loading-local-json-file:
-    
-    /* //LOCAL DEBUG CODE - simulating get form server 
-    jdata = [
-			    {"name": "1% Milk", "score" : 0},
-			    {"name": "VM Scrambled Eggs", "score" : 0},
-			    {"name": "Vanilla Soft Serve", "score" : 0},
-			    {"name": "Vegan No Dairy Cheese Pizza", "score" : 0},
-			    {"name": "White Hamburger Roll", "score" : 0},
-			    {"name": "Whole Eggs", "score" : 0},
-			    {"name": "Whole Milk", "score" : 0},
-			    {"name": "Whole Wheat Bread", "score" : 0},
-			    {"name": "Whole Wheat Cheese Pizza", "score" : 0},
-			    {"name": "Whole Wheat English Muffin", "score" : 0},
-			    {"name": "Yellow American Cheese", "score" : 0},
-			    {"name": "Yellow Delicious Apples", "score" : 0}
-			];
-	*/
+var url = "https://fast-coast-3052.herokuapp.com/"
 
-	    $.each( jdata, function (i, food_obj) {
-
-	    $('#data').append(
-	    	"<div class='row'>" +
-	    	"<div class='col-xs-6 > <p class='fooditem'>" + food_obj.name + "</p></div>" +
-	    	"<div class='col-xs-6 > <p class='foodscore'>" + (food_obj.score).toString() + "</p></div>" +
-	    	"</div>")
-	    });
-  	console.log(jdata); // this will show the info it in firebug console
-
-  	});
+function init(){
+    var  list = [];
+    $.getJSON(url, function(data){
+        console.log("after get");
+        console.log(data)
+        $.each(data, function(key, val){
+            obj = {"name" : key, "score" : val};
+            list.push(obj);
+        });
+        console.log(list);
+        list.sort(compare);
+        renderList(list);
+    });
 }
 
-// TODO -- Getting arrow pictures for each topic onto list.
-/*IDEA -- Place each item in a Div
-*/
+function renderList(list){
+        $.each(list, function (i, food_obj) {
+        $('#data').append(
+            "<div class='row'>" +
+            "<div class='col-xs-6 > <p class='fooditem'>" + food_obj.name + "</p></div>" +
+            "<div class='col-xs-6 > <p class='foodscore'>" + (food_obj.score).toString() + "</p></div>" +
+            "</div>")
+        });
+        console.log(data); // this will show the info it in firebug console
+}
+
+// compares two food items in the form
+// {"name": name, "score": x}
+// i.e. a greater than function
+function compare(obj1,obj2){
+    return obj2.score - obj1.score;
+}
