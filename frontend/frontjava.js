acaccb172e1db9e2b9f2aad599436be67cf08717
Param Chopra <param012chopra@gmail.com>
2015-04-09 23:11:29 -0400

    resolved merge conflicts

--- frontend/frontjava.js
+++ frontend/frontjava.js
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
 var url = "https://fast-coast-3052.herokuapp.com/"
-=======
-function getdata(){
-    $.getJSON("https://fast-coast-3052.herokuapp.com/list", function(jdata) { //code based off of stackoverflow.com/questions/7346563/loading-local-json-file:
-    
-    /* //LOCAL DEBUG CODE - simulating get form server 
-    jdata = [
-			    {"name": "1% Milk", "score" : 0},
-			    {"name": "VM Scrambled Eggs", "score" : 0},
-			    {"name": "Vanilla Soft Serve", "score" : 0},
-			    {"name": "Vegan No Dairy Cheese Pizza", "score" : 0},
-			    {"name": "White Hamburger Roll", "score" : 0},
-			    {"name": "Whole Eggs", "score" : 0},
-			    {"name": "Whole Milk", "score" : 0},
-			    {"name": "Whole Wheat Bread", "score" : 0},
-			    {"name": "Whole Wheat Cheese Pizza", "score" : 0},
-			    {"name": "Whole Wheat English Muffin", "score" : 0},
-			    {"name": "Yellow American Cheese", "score" : 0},
-			    {"name": "Yellow Delicious Apples", "score" : 0}
-			];
-	*/
-		console.log(jdata)
->>>>>>> 65540f4e71544b4805d6a191c54bf56827f9635d
 
 function init(){
     var  list = [];
