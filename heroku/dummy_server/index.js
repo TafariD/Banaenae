2f1e9867ba11141043bcc72fd1a036f9de03f46d
Jason Fan <jason.fan.74@gmail.com>
2015-04-09 22:04:03 -0400

    ordered list up on /list

--- heroku/dummy_server/index.js
+++ heroku/dummy_server/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 
-json = require('./foods.json');
+dict = require('./foods.json');
+list = require('./list.json');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(json);
+	response.send(dict);
+});
+
+app.get('/list',function(request,response) {
+	response.send(list);
 });
 
 app.listen(app.get('port'), function() {
