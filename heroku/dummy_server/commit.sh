64507e34f37c5df3a6de72976d29e1184e259e37
Jason Fan <jason.fan.74@gmail.com>
2015-04-09 21:28:43 -0400

    testing heroku push

--- heroku/dummy_server/commit.sh
+++ heroku/dummy_server/commit.sh
@@ -1,7 +1,7 @@
 #Tyler Lubeck's Heroku Commit Script
 #	-works around only having one repo
 
-HEROKU_APP="../../../comp20-dummy_server/"
+HEROKU_APP="../../../comp20-dummy_server"
 GROUP_PROJECT=`pwd`
 
 cp -r * $HEROKU_APP
@@ -17,7 +17,7 @@ if [ "$1" =  "-commit" ];then
 
 	cd $HEROKU_APP
 
-    ##git pull
+    #git pull
 	git add *
 	git commit -m "$COMMITMESSAGE"
 	git push origin master
