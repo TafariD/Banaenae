e086828991b3d95cdfd3a1629478b37e95f77d42
Jason Fan <jason.fan.74@gmail.com>
2015-04-09 22:21:31 -0400

    cors access

--- heroku/dummy_server/index.js
+++ heroku/dummy_server/index.js
@@ -15,9 +15,9 @@ app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
- });
+});
 
-app.get('/list',function(request,response) {
+app.get('/list',function(request,response,next) {
 	response.send(list);
 });
 
