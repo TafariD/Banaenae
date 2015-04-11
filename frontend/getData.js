3eb20f891fcdc4d9765b723809696b0dff0ed9f0
Jason Fan <jason.fan.74@gmail.com>
2015-04-11 19:38:21 -0400

    front => set up divs for up and downvote

--- frontend/getData.js
+++ frontend/getData.js
@@ -16,8 +16,11 @@ function renderList(list){
         $.each(list, function (i, food_obj) {
         $('#data').append(
             "<div class='row'>" +
-            "<div class='col-xs-6 > <p class='fooditem'>" + food_obj.name + "</p></div>" +
-            "<div class='col-xs-6 > <p class='foodscore'>" + (food_obj.score).toString() + "</p></div>" +
+            "<div class='col-xs-8 left' > <p class='fooditem'>" + food_obj.name + "</p> </div>" +
+            "<div class='col-xs-4 right'> "
+                    + "<div class='col-xs-12'> <p> ^ </p> </div>"
+                    + "<div class='col-xs-12'> <p class='foodscore'>" + food_obj.score.toString() + "</p></div>"
+                    + "<div class='col-xs-12'> <p> v </p> </div> </div>" +
             "</div>")
         });
 }
