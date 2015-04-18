e0984b39b22b2498fb0b8f2c7d63089acab25556
Jason Fan <jason.fan.74@gmail.com>
2015-04-17 21:59:28 -0400

    fooditems schema and load built

--- app/routes.js
+++ app/routes.js
@@ -14,17 +14,29 @@ module.exports = function(app, passport) {
     // SCORES
     app.get('/scores', isLoggedIn, function(req, res) {
         // TEST CODE - Passing JSON in plaintext to .ejs template
-        var scores =    [{"name":"1% Milk","score":60},
-                     {"name":"VM Scrambled Eggs","score":15},{"name":"Vanilla Soft Serve","score":-10}];
+       // var scores =    [{"name":"1% Milk","score":60},
+         //            {"name":"VM Scrambled Eggs","score":15},{"name":"Vanilla Soft Serve","score":-10}];
         // TEST CODE
-        // We want scores = getScores() <---- grabs data from mongoDB
-        res.render('displayitems.ejs', { user: req.user, foodScores : JSON.stringify(scores)});
+        getScores(); //<---- grabs data from mongoDB
+        var query = foodItem.find({"daily" : true});
+        query.select("id name daily_score").sort({"daily_score":-1}).exec(function(err, docs){
+            res.render('displayitems.ejs', { user: req.user, foodScores : JSON.stringify(docs)});
+        });        
     });
 
     // For testing -  we can add foods via html post
     app.post('/addfood', function(req, res) {
-        console.log(req.body.foodname, req.body.score);
-        res.send(addFood(req));
+        if (req.body.foodname == undefined || req.body.foodname == "" || 
+            req.body.score == "" ||req.body.score == undefined) {
+            res.send("bad input :(");
+        } else {
+            addFood(req, res);
+        }
+    });
+
+    app.get('/scorestest', function(req,res) {
+        getScores();
+        res.send(200);
     });
 
     // LOGOUT 
@@ -32,6 +44,7 @@ module.exports = function(app, passport) {
         req.logout();
         res.redirect('/');
     });
+
     // GOOGLE ROUTES
     // send to google to do the authentication
     app.get('/auth/google', passport.authenticate('google', { scope : ['profile'] }));
@@ -39,39 +52,39 @@ module.exports = function(app, passport) {
     app.get('/auth/google/callback',
             passport.authenticate('google', {
                     successRedirect : '/scores',
-                    failureRedirect : '/'
+                    failureRedirect : '/' 
             })
     );
 };
 
+function getScores() {
+    var query = foodItem.find({"daily" : true});
+    query.select("id name daily_score").sort({"daily_score":-1}).exec(function(err, docs){
+        return docs;
+    });
+}
+
 // Add data from addfood
-
-function addFood(req) {
-    if (req.body.foodname != undefined && req.body.score != undefined) {
-        var toUpdate = {
-                    name            : req.body.foodname,        
-                    daily           : true,
-                    daily_score     : parseInt(req.body.score, 10),
-                    alltime_score   : parseInt(req.body.score, 10),
-                    carm            : true,
-                    dewick          : true,
-                    stats : {
-                        freq : parseInt(req.body.score, 10)          
-                    }
-            };
-
-        foodItem.update({"name" : req.body.foodname}, toUpdate, {upsert : true});
-        return "Success!";
-    } else {
-        return "add Item failed :(";
+function addFood(req, res) {
+    var toUpdate ={
+                name            : req.body.foodname,        
+                daily           : true,
+                daily_score     : parseInt(req.body.score, 10),
+                alltime_score   : parseInt(req.body.score, 10),
+                carm            : true,
+                dewick          : true,
+                stats           : {freq : 1}
     }
+    foodItem.update({"name" : req.body.foodname}, toUpdate, {upsert : true}, function (err, num, raw) {
+        console.log("in update");
+        if (err) console.log("error in update");
+        res.send("{'status':'success!'}");
+    });
 }
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-    // if user is authenticated in the session, carry on 
     if (req.isAuthenticated())
         return next();
-    // if they aren't redirect them to the home page
     res.redirect('/');
 }
