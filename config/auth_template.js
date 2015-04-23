6bf1d4d35cea9ac41ba27c64266efd3afc96843e
Jason Fan <jason.fan.74@gmail.com>
2015-04-23 17:18:23 -0400

    tafari test button

---
+++ config/auth_template.js
@@ -0,0 +1,8 @@
+// config/auth.js
+module.exports = {
+    'googleAuth' : {
+        'clientID'      : 'YOUR CLIENT ID HERE',
+        'clientSecret'  : 'YOUR CLIENT SECRET HERE',
+        'callbackURL'   : 'calback url here'
+    }
+};
