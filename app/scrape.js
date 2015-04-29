fe57b799ec403855016616a938abf7dfb234fa86
Jason Fan <jason.fan.74@gmail.com>
2015-04-29 19:47:45 -0400

    features => buttons more funcitonal

--- app/scrape.js
+++ app/scrape.js
@@ -19,31 +19,23 @@ module.exports = function(schedule, mongoose) {
 }
 // Returns a list of strings of daily items
 function grabDailyItems() {
-    console.log('in grabDailyItems');
     var url = 'http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts+Dining&locationNum=11&locationName=Dewick+MacPhie+Dining+Center&naFlag=1';
     request(url, function(error, response, html){
-        console.log('in request');
         if (!error) {
-            console.log('about to cheerio load');
             var $ = cheerio.load(html);
-            console.log('cheerio loaded')
             var items = $(".shortmenurecipes").find("a");
-            //var items = [];
-            console.log('got items, now in forloop')
             $(items).each(function(){
-                //console.log("I've got all the items");
                 console.log($(this).text())
                 itemToDB($(this).text());
-                //items.push($(this).text());
             });
-            //console.log(items);
         } else {
             console.log('error in request');
         }
     });
-    console.log('out of request');
 }
 
+// string
+// add foodname / foodItem to DB,
 function itemToDB(foodname) {
     foodItem.findOne({'name' : foodname}, function(err, item){
         if(err) {
@@ -65,17 +57,3 @@ function itemToDB(foodname) {
         }
     });
 }
-
-/* psuedo:
-for each item:
-    var toUpdate = {
-                name            : req.body.foodname,        
-                daily           : true,
-                daily_score     : parseInt(req.body.score, 10),
-                alltime_score   : parseInt(req.body.score, 10),
-                carm            : true,
-                dewick          : true,
-                stats           : {freq : 1}
-    }
-*/
-
